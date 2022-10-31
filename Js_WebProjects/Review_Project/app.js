1910ed36ffa02a471e68d8f362bf7bf28bedd14f
Labbi Ahmed <30775026+Labbi-Ahmed@users.noreply.github.com>
2022-10-31 07:22:46 +0600

    change the images

--- Js_WebProjects/Review_Project/app.js
+++ Js_WebProjects/Review_Project/app.js
@@ -3,8 +3,8 @@ const reviews = [
         id: 1,
         name: 'lady',
         job: 'web developer',
-        img: 
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+        img:
+            "	https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
         text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
 
     },
@@ -12,8 +12,8 @@ const reviews = [
         id: 2,
         name: 'Anna Johnson',
         job: 'WEB DESIGNER',
-        img: 
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+        img:
+            "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
         text:
         "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel\
         pin fashion axe.photo booth jean shorts artisan narwhal."
