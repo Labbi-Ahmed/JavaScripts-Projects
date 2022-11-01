212c511fc10301f9c9f7a43e99f96fce2db26829
Labbi Ahmed <30775026+Labbi-Ahmed@users.noreply.github.com>
2022-11-01 22:10:11 +0600

    Question and answer done

--- Js_WebProjects/QA/app.js
+++ Js_WebProjects/QA/app.js
@@ -1,2 +1,34 @@
 //using selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question){
+    // console.log(question)
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click',function(){
+
+        questions.forEach(function(item){
+            if(item !== question)item.classList.remove('show-text'); 
+        })
+
+
+        question.classList.toggle('show-text')
+    })
+
+})
+
+
+
+
+
+
+
 // traversing the dom
+// btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(funciton(btn){
+//     btn.addEventListener('click', function(e){
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text')
+//     });
+    
+// });
