7c3539eb3646cd29dbe1c33a0ffa11102187de99
Labbi Ahmed <30775026+Labbi-Ahmed@users.noreply.github.com>
2022-11-01 08:21:29 +0600

    optimize toggle option by .classList.toggle

--- Js_WebProjects/SideBar/app.js
+++ Js_WebProjects/SideBar/app.js
@@ -1,10 +1,13 @@
 const navToggle = document.querySelector('.nav-toggle');
-const links = document.querySelector('.sideBar');
+const sideBar = document.querySelector('.sideBar');
 const faTimes = document.querySelector('.fa-times');
 navToggle.addEventListener('click', function(){
-    const toggle = links.classList.contains('show-sideBar');
-    (toggle) ? links.classList.remove('show-sideBar') : links.classList.add('show-sideBar')
+    // const toggle = links.classList.contains('show-sideBar');
+    // (toggle) ? links.classList.remove('show-sideBar') : links.classList.add('show-sideBar')
+    sideBar.classList.toggle('show-sideBar');
+    console.log(sideBar.classList);
 })
 faTimes.addEventListener('click', function(){
-    links.classList.remove('show-sideBar');
+    sideBar.classList.remove('show-sideBar');
+    console.log(sideBar.classList);
 })
